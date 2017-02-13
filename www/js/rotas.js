a130134c716c5bb4d130f615445f38a99259f9e1
huggosantos <huggo.oliveira1@gmail.com>
2017-02-13 15:08:33 -0300

    teste 2

--- www/js/rotas.js
+++ www/js/rotas.js
@@ -66,19 +66,102 @@ var datasJogos = new Array();// vetor que guarda as datas dos jogos das apostas;
 var ultimaAposta;//variavel q guarda a ultima aposta do cambista;
 
 
-//----------Controller que faz a validação das apostas feitas pelo app cliente.------------------------------
-app.controller('validarAposta', function($scope, $http, $route, $location) { 
+//Controler parar gerenciar tela onde o agente podera escolher entre compartilhar ou imprimir bet
+app.controller('comprovante', function($scope, $http, $route, $location) { 
 
-  $scope.rodar = function(){
-    html2canvas($('#print'),{
-      onrendered: function(canvas) {
-        var img = canvas.toDataURL();
+$scope.jsonComprovante=jsonApostas;
+//console.log("comprovante"+jsonApostas.aposta.apostador);
+$scope.enviarWhatsApp = function(){
+  html2canvas($('#print'),{
+    onrendered: function(canvas) {
+      var img = canvas.toDataURL();
           //window.open(img);
-          window.plugins.socialsharing.shareViaWhatsApp(' Teste envio de Mensagem e Img via WhatsApp',img, null , function () {alert( 'share ok')}, function ( errormsg) {alert (errormsg)});
+          window.plugins.socialsharing.shareViaWhatsApp('Sua Aposta no BETSOCCER',img, null , function () {alert( 'Aposta Compartilhada')}, function ( errormsg) {alert ("Erro ao tentar Compartilhar!")});
         }
       });
+}  
 
-  }   
+$scope.verificaStatusAposta = function(statusAposta){
+  if(statusAposta==true){
+    return "Valida";
+  }else{
+    return "Aguardando Validação";
+  }
+}
+
+$scope.apostaVencedora = function(statusAposta){
+  if(statusAposta==true){
+    return "Sim";
+  }else{
+    return "Não";
+  }
+}
+
+
+$scope.retornaValorPalpite = function(ob){
+  for(var i in jsonApostas.palpites){
+    if(ob==jsonApostas.palpites[i].jogos_id){
+      var x = jsonApostas.palpites[i].palpite;
+      return x;
+    }
+  }
+}
+
+
+$scope.retornaPalpite = function (ob){
+  console.log("ob"+ob);
+  for(var i in jsonApostas.palpites){
+    if(ob==jsonApostas.palpites[i].jogos_id){
+      var x;
+      if(jsonApostas.palpites[i].tpalpite=="valor_casa"){
+        x="Casa";
+        return x;
+      }else if(jsonApostas.palpites[i].tpalpite=="valor_fora"){
+        x="Fora";
+        return x;
+      } else if(jsonApostas.palpites[i].tpalpite=="valor_empate"){
+        x="Empate";
+        return x;
+      } else if(jsonApostas.palpites[i].tpalpite=="valor_dupla"){
+        x="Dupla";
+        return x;
+      } else if(jsonApostas.palpites[i].tpalpite=="valor_1_2"){
+        x="Gol 1/2";
+        return x;
+      } else if(jsonApostas.palpites[i].tpalpite=="min_gol_3"){
+        x="+2.5";
+        return x;
+      } else if(jsonApostas.palpites[i].tpalpite=="max_gol_2"){
+        x="-2.5";
+        return x;
+      } else if(jsonApostas.palpites[i].tpalpite=="ambas_gol"){
+        x="Ambas";
+        return x;
+      }
+    }
+  }
+}
+toTop();
+$scope.toData = function(dateTime) {  
+  var dateTime = dateTime.split(" "); //Cria um array com uma posição ["2016-07-10 12:40:10"]
+  var date = dateTime[0].split("-"); //Separa A string aprtir do "-" Cria um Array com tres posições ["2016", "17", "10"]
+  var dataFinal = date[2] + "/" +
+  date[1];
+  return dataFinal; //Retona a data No Padrao Brasileiro ["10/17/2016"]
+}
+ //Metodo que faz um split em string DataTime e retonar apenas a Hora
+ $scope.toHora = function(dateTime) {
+  var dateTime = dateTime.split(" "); //Cria um array com uma posição ["2016-07-10 12:40:10"]
+  var time = dateTime[1].split(":"); //Separa a string aprtir do ":" Cria um Array com tres posições ["12", "40", "10"]
+  var timeFinal = time[0] + ":" + time[1];
+  return timeFinal; //Retona a hora descosiderando os segundos ["12:40"]
+}
+
+});
+
+
+//----------Controller que faz a validação das apostas feitas pelo app cliente.------------------------------
+app.controller('validarAposta', function($scope, $http, $route, $location) { 
 
   $(document).ready(function(){
         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
@@ -163,7 +246,6 @@ $scope.BuscarAposta = function() {
   });
 }
 
-
 $scope.verificaStatusAposta = function(statusAposta){
   if(statusAposta==true){
     return "Valida";
@@ -180,7 +262,6 @@ $scope.apostaVencedora = function(statusAposta){
   }
 }
 
-
 $scope.retornaValorPalpite = function(ob){
   for(var i in aptStatus.palpites){
     if(ob==aptStatus.palpites[i].jogos_id){
@@ -190,42 +271,11 @@ $scope.retornaValorPalpite = function(ob){
   }
 }
 
-$scope.retornaPalpite = function (ob){
-
-  for(var i in aptStatus.palpites){
-    if(ob==aptStatus.palpites[i].jogos_id){
-      var x;
-      if(aptStatus.palpites[i].tpalpite=="valor_casa"){
-        x="Casa";
-        return x;
-      }else if(aptStatus.palpites[i].tpalpite=="valor_fora"){
-        x="Fora";
-        return x;
-      } else if(aptStatus.palpites[i].tpalpite=="valor_empate"){
-        x="Empate";
-        return x;
-      } else if(aptStatus.palpites[i].tpalpite=="valor_dupla"){
-        x="Dupla";
-        return x;
-      } else if(aptStatus.palpites[i].tpalpite=="valor_1_2"){
-        x="Gol 1/2";
-        return x;
-      } else if(aptStatus.palpites[i].tpalpite=="min_gol_3"){
-        x="+2.5";
-        return x;
-      } else if(aptStatus.palpites[i].tpalpite=="max_gol_2"){
-        x="-2.5";
-        return x;
-      } else if(aptStatus.palpites[i].tpalpite=="ambas_gol"){
-        x="Ambas";
-        return x;
-      }
-    }
-  }
-}
-toTop();
 
 });
+
+
+
 //--------------------------FIM VALIDAÇÂO DE CODIGO DE APOSTA DO APP CLIENT-------------------------------------------------
 
 //Controller para realizar o acerto com os agentes, pra consumir o servico é necessario o cod. Seguranca do cambista e do ADM.
